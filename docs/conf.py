600a97ec1f8eab5966b6542b1084dd718b6c5377
Rafal Gumienny <r.gumienny@unibas.ch>
2016-06-15 16:03:16 +0200

    build working

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['drmaa', 'numpy', 'pandas']
+MOCK_MODULES = ['drmaa', 'drmaa.errors', 'numpy', 'pandas', 'mysql-python', 'statsmodels', 'statsmodels.api']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
